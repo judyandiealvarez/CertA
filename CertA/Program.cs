0508de7d9ce2820728c6c5542c6b7312794cdba0
Judy Alvarez <judy.andie.alvarez@icloud.com>
2025-08-20 12:49:46 +0300

    Fix restore: pin Serilog.Sinks.PostgreSQL to 2.3.0 and configure column writers

--- CertA/Program.cs
+++ CertA/Program.cs
@@ -4,6 +4,9 @@ using CertA.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+using Serilog.Sinks.PostgreSQL.ColumnWriters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +14,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((ctx, lc) =>
 {
     var conn = ctx.Configuration.GetConnectionString("DefaultConnection");
-    lc.ReadFrom.Configuration(ctx.Configuration)
+    var columnWriters = new Dictionary<string, ColumnWriterBase>
+    {
+        ["id"] = new SerialColumnWriter(),
+        ["timestamp"] = new TimestampColumnWriter(),
+        ["level"] = new LevelColumnWriter(renderAsText: true),
+        ["message"] = new MessageTemplateColumnWriter(),
+        ["message_rendered"] = new RenderedMessageColumnWriter(),
+        ["exception"] = new ExceptionColumnWriter(),
+        ["properties"] = new LogEventSerializedColumnWriter(),
+        ["event_id"] = new EventIdColumnWriter()
+    };
+
+    lc.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+      .MinimumLevel.Override("System", LogEventLevel.Warning)
+      .MinimumLevel.Information()
       .WriteTo.Console()
       .WriteTo.PostgreSQL(
           connectionString: conn!,
           tableName: "application_logs",
+          columnOptions: columnWriters,
           needAutoCreateTable: true);
 });
 
